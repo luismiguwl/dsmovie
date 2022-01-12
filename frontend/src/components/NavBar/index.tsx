ca26c02e2427e3ffc59418b0a28bdb8a125169b8
luismiguwl <luisbwrb@gmail.com>
2022-01-12 14:47:30 -0300

    Navbar

---
+++ frontend/src/components/NavBar/index.tsx
@@ -0,0 +1,22 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import './styles.css';
+
+function NavBar() {
+    return (
+        <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a href="https://github.com/luismiguwl">
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/luismiguwl</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+    );
+}
+
+export default NavBar;
