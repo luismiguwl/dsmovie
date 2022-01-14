c564a4e7f5bc958fc5689903ebcbbf89591f4684
luismiguwl <luisbwrb@gmail.com>
2022-01-14 01:11:04 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/luis/dsmovie/entities/Score.java
@@ -0,0 +1,34 @@
+package com.luis.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+	
+	@EmbeddedId
+	private ScorePk id = new ScorePk();
+	private Double value;
+	
+	public Score() {
+	}
+
+	public ScorePk getId() {
+		return id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+	
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+	
+}
